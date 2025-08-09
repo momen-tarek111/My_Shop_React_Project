0573cb53facce2363fe6c6b6273800e7577c3536
momen-tarek111 <momen.tarek.nagaty@gmail.com>
2025-08-09 03:02:13 +0300

    final 2

--- src/redux/apiCalls/productAPICalls.js
+++ src/redux/apiCalls/productAPICalls.js
@@ -3,7 +3,8 @@ import { productActions } from "../slices/productSlices";
 export function fetchProducts() {
     return async (dispatch) => {
         try {
-            const response = await fetch("http://localhost:5000/products")
+            // const response = await fetch("http://localhost:5000/products")
+            const response = await fetch("https://my-json-server.typicode.com/momen-tarek111/json_server_My_Shop_Project/products")
             const data = await response.json()
             dispatch(productActions.setProducts(data))
         } catch (error) {
@@ -16,7 +17,8 @@ export function getProductById(productId) {
     return async (dispatch) => {
         try {
             dispatch(productActions.setLoading())
-            const response = await fetch(`http://localhost:5000/products/${productId}`)
+            // const response = await fetch(`http://localhost:5000/products/${productId}`)
+            const response = await fetch(`https://my-json-server.typicode.com/momen-tarek111/json_server_My_Shop_Project/products/${productId}`)
             const data = await response.json()
             dispatch(productActions.setProduct(data))
             dispatch(productActions.clearLoading())
